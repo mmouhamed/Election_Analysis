e2f463c8db2d58ae62e488410441136ae1c9d426
Mouhari Mouhamed <mmouhamed@healthemed.com>
2024-10-23 14:21:23 -0500

    Adding the election analysis.

--- PyPoll.py
+++ PyPoll.py
@@ -13,24 +13,82 @@ import os
 
 # Initiation File to read and file to Save
 file_to_load = os.path.join("Resources","election_results.csv") 
-file_to_save = os.path.join("analysis", "election_analysis.txt")
+file_to_save = os.path.join("analysis", "election_analysis_test.txt")
 
+total_vote= 0
+
+candidate_options = []
+candidate_votes = {}
+
+winning_count= 0
+wiining_percentage = 0
+winning_candidate = ""
 
 # Opening File
 
-with open(file_to_load) as election_data:
-    file_reader = csv.reader(election_data)
-    headers = next(file_reader)
-    print(headers)
+with open(file_to_load) as file_data:
+    # File store data as a list of lists
+    file_reader = csv.reader(file_data)
+    #Skip to header of the data
+    header = next(file_reader)
+    # print(header)
     
+    #Iterate over the list and tally total votes
+    for row in file_reader:
+        total_vote+=1
+        candidate_name = row[2]
+
+        if candidate_name not in candidate_options:
+            candidate_options.append(candidate_name)
+            candidate_votes[candidate_name] = 0
+        candidate_votes[candidate_name] +=1
     
-    
-    
-    # with open(file_to_save, 'a') as save_file:
-    #     for row in file_reader: 
-    #         save_file.write(row[1])
+    with open(file_to_save, "w") as txt_file:
+        election_result = (
+            f"Election Results\n"
+            f"----------------------------------\n" 
+            f"Total Votes: {total_vote:,}\n"
+            f"----------------------------------\n" 
+
+        )
+        txt_file.write(election_result)
+
+        for candidate_name in candidate_votes:
+            votes = candidate_votes[candidate_name]
+            votes_percentage = float(votes)/float(total_vote)*100
+            
+            canditate_result = (f"{candidate_name}: {votes_percentage:.1f} ({votes:,})\n")
+            txt_file.write(canditate_result)
+            if votes > winning_count:
+                winning_count = votes
+                winning_candidate = candidate_name
+                wiining_percentage = votes_percentage
+
+        winning_candidate_result = (
+            f"----------------------------------\n" 
+            f"Winner: {winning_candidate} \n"
+            f"Winning Vote Count: {winning_count:,} \n"
+            f"Winning Percentage: {wiining_percentage:.1f}% \n"
+            f"----------------------------------\n" 
+
+        ) 
+
+        txt_file.write(winning_candidate_result)
+
+
+
+
         
 
+
+
+
+print(total_vote)
+print(candidate_options)
+print(candidate_votes)
+print(winning_count)
+print(wiining_percentage)
+print(winning_candidate)
 
 
 
